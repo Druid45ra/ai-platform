c32de913cb39040b4c002f279567b4dbfaa4af0b
Druid45ra <radu_vanca@live.com>
2025-02-24 15:26:28 +0200

    Add SQLAlchemy base class and update requirements for scikit-learn

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,6 +4,8 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from app.db.base_class import Base
+target_metadata = Base.metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
